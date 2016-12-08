e2fb069703a8fdd581ab06f8a5a7584a78d95cee
the7winds <vga.grc@gmail.com>
2016-12-08 19:35:38 +0000

    added new type

--- src/Builtin.ml
+++ src/Builtin.ml
@@ -1,12 +1,16 @@
 
 module Value =
   struct
-    type t = Int of int | String of bytes
+    type t = Int of int | String of bytes | Array of t array
 
     let of_int x = Int x
-    let of_string b = String b
     let to_int (Int x) = x
+
+    let of_string b = String b
     let to_string (String b) = b
+
+    let to_array a = Array a
+    let of_array (Array a) = a
 
     let str x = 
         match x with
